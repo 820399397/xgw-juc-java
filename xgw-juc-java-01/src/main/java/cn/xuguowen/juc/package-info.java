03237e37c38401c35671f29f6a89dfd04553394b
xuguowen <820399397@qq.com>
2024-05-15 10:48:35 +0800

    feat:chapter01

---
+++ xgw-juc-java-01/src/main/java/cn/xuguowen/juc/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * ClassName: package-info
+ * Package: cn.xuguowen.juc
+ * Description:学习满一航老师的Java并发编程课程。
+ *             课程地址链接：https://www.bilibili.com/video/BV16J411h7Rd/?p=8&spm_id_from=pageDriver&vd_source=e4f9ba4de74f523b6e0beb462a1b2107
+ *             视频学习完之后，然后会通过书籍的方式再去巩固一遍，巩固完之后，会通过博客的方式，把学习到的知识，用自己的话描述一遍，形成自己的知识体系。
+ *             最后，通过博客的方式，去分享自己的知识，分享的过程就是巩固的过程。
+ *
+ * @Author 徐国文
+ * @Create 2024/5/15 10:45
+ * @Version 1.0
+ */
+package cn.xuguowen.juc;
