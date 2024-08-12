f2275c7236af995d53a605408c85458b83da3dbc
xuguowen <820399397@qq.com>
2024-08-12 12:12:51 +0800

    feat:ReentrantLock的原理

---
+++ xgw-juc-java-08/src/main/java/cn/xuguowen/juc/ReentrantLockPrinciple.java
@@ -0,0 +1,21 @@
+package cn.xuguowen.juc;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * ClassName: ReentrantLockPrinciple
+ * Package: cn.xuguowen.juc
+ * Description:ReentrantLock加锁原理
+ * 1.加锁 解锁原理
+ * 2.可重入原理
+ *
+ * @Author 徐国文
+ * @Create 2024/8/1 22:59
+ * @Version 1.0
+ */
+public class ReentrantLockPrinciple {
+
+    public static void main(String[] args) {
+        ReentrantLock lock = new ReentrantLock();
+    }
+}
